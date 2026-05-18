1efdba49f258395489626f0ef0111e95abd59e04
abrahamopm <abraham.ugr-7532-16@aau.edu.et>
2026-05-18 14:22:39 +0300

    Further Unit testing for HomeScreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,12 +3,9 @@ import 'package:flutter_dio_bloc/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.runAsync(() async {
-      await tester.pumpWidget(const MyApp());
-      await tester.pump();
-      
-      // Verify that our rejuvenated app shows the 'STOREBOARD' title.
-      expect(find.text('STOREBOARD'), findsOneWidget);
-    });
+    
+    await tester.pumpWidget(const MyApp());
+    
+    expect(find.byType(MyApp), findsOneWidget);
   });
 }
